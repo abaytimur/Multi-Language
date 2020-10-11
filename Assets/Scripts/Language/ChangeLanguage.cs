d02990b1bd8475ef770f18f34ba995104c87966e
AtaYanki <54235883+AtaYanki@users.noreply.github.com>
2020-10-11 19:01:06 +0300

    Add files via upload

---
+++ Assets/Scripts/Language/ChangeLanguage.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeLanguage : MonoBehaviour
+{
+    public string[] myLangs;
+	int index;
+
+	public void ChangeLanguageType(int myLanguage)
+	{
+		index = myLanguage;
+		PlayerPrefs.SetInt ("_language_index", index);
+		PlayerPrefs.SetString ("_language", myLangs[index]);
+		ApplyLanguageChanges();
+	}
+
+	void ApplyLanguageChanges ()
+	{
+		SceneManager.LoadScene(0);
+	}
+}
---
+++ Assets/Scripts/Language/Translator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Translator : MonoBehaviour
+{
+	[Tooltip ("enter one of the keys that you specify in your (txt) file for all languages.\n\n# for example: [HOME=home]\n# the key here is [HOME]")]
+	public string key;
+
+	void Start ()
+	{
+		GetComponent<Text>().text = Language.GetTraduction(key);
+	}
+}
